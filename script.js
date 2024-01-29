47f36057c50ac34b975c8b10ce88a8387aff74b5
Gabs <40191213+GabZoFar@users.noreply.github.com>
2024-01-29 21:06:15 +0100

    fixed issues with btn, changed styling to red

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-function boldenText() {
+window.boldenText = function() {
     const inputText = document.getElementById('input').value;
     const output = document.getElementById('output');
     const boldenedText = inputText.split('\n').map((line) => {
         return line.split(' ').map((word) => {
             if (word.length > 6) {
-                return \`<b>\${word.slice(0, 4)}</b>\${word.slice(4)}\`;
+                return `<b>${word.slice(0, 4)}</b>${word.slice(4)}`;
             } else {
-                return \`<b>\${word.slice(0, 3)}</b>\${word.slice(3)}\`;
+                return `<b>${word.slice(0, 3)}</b>${word.slice(3)}`;
             }
         }).join(' ');
     }).join('\n');
